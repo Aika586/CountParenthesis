59c4668f392eb2544af72c43a1a5f97071b4a237
Aikokul <baatirbekovna.6@mail.ru>
2025-05-26 15:48:09 +0600

    added countParenthesis with tests

---
+++ __tests__/perfomance.test.js
@@ -0,0 +1,21 @@
+const { countWellFormedParenthesis }=require("../countParenthesis.js");
+
+
+const { performance } = require('perf_hooks');
+
+describe('Performance tests', () => {
+    test('performance test n = 15', () => {
+        const start = performance.now();
+        const result = Math.floor(countWellFormedParenthesis(15));
+        const end = performance.now();
+        const duration = end - start;
+
+        console.log(`n=15 => result = ${result}, time = ${duration.toFixed(4)}ms`);
+
+        const expectedMaxTimeMs = 1000; 
+        expect(duration).toBeLessThan(expectedMaxTimeMs);
+        expect(result).toBe(9694844);
+    });
+});
+
+
